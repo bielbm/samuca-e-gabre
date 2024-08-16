0e9e205961fc38b431474847c87f23c6e704ac16
bielbm <163012253+bielbm@users.noreply.github.com>
2024-08-16 10:30:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function exercicio1(){
+    let x = prompt("Digite um numero entre 0 e 10");
+    let i;
+    for( i = 0; i > 1;i++ ) {
+    if(x < 0){
+     prompt("numero invalido");
+
+    } 
+    else(x >10) 
+
+     console.log("numero invalido");
+    
+    }
+
+        
+
+}
