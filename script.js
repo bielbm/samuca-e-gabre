cd50b4dad4620c57486a026ea6a160d5e24b355b
bielbm <163012253+bielbm@users.noreply.github.com>
2024-08-24 12:06:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,69 @@
 function exercicio1(){
-let nota = prompt("Digite uma nota de 1 a 10:");
-while (nota>10 || nota<1){
-    nota = parseInt(prompt("Valor inválido, digite novamente"));
+    let nota = prompt("Digite uma nota de 1 a 10:\n");
+    console.log("A nota digitada foi essa\n", nota);
+    while (nota>10 || nota<1 || isNaN(nota) || nota.trim() ===''){
+        nota = parseInt(prompt("Valor inválido, digite novamente\n"));
+    }
+    alert("Nota válida\n");
+    console.log("Nota válida\n");
 }
-        
 
+function exercicio2(){
+    let NomeDoUsuario = prompt("Digite seu nome:\n");
+    let SenhaDoUsuario = prompt("Digite a sua senha:\n");
+  
+    while(NomeDoUsuario.trim()===''){
+       NomeDoUsuario = prompt("Valor vazio, digite novamente:\n");
+    }
+    while(SenhaDoUsuario.trim()==='' || SenhaDoUsuario==NomeDoUsuario){
+       SenhaDoUsuario = prompt("Valor vazio, por favor digite sua senha novamente:\n");
+    }
+    alert("Cadastro aceito");
+    console.log("Cadastro aceito");
+}
+  
+function exercicio3(){
+    let nota = prompt("Digite o número de notas: ");
+    while(nota<1 || nota.trim()==='' || isNaN(nota)){
+      nota = prompt("Valor inválido digite novamente: ");
+    }
+    nota = parseInt(nota);
+    let soma = 0;
+    
+    for (let i = 0; i < nota; i++) {
+    let nota = prompt(`Digite a nota ${i + 1}: `);
+    while(nota<0 || nota.trim()==='' || isNaN(nota)){
+      nota = prompt("Nota inválida, digite novamente: ");
+      
+    }
+    nota = parseFloat(nota);
+    soma += nota;
+    }
+    
+    let media = soma / N;
+    
+    alert(`A média aritmética é: ${media.toFixed(2)}`);
+      
+}
+    
+function exercicio4(){
+    const numero = parseInt(prompt("Digite um número inteiro: "));
+      
+    function ehPrimo(numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    for (let i = 2; i * i <= numero; i++) {
+    if (numero % i === 0) {
+    return false;
+    }
+    }
+    return true;
+    }
+      
+    if (ehPrimo(numero)) {
+    console.log(`${numero} é um número primo!`);
+    } else {
+    console.log(`${numero} não é um número primo.`);
+    }
 }
